0be475b562c8351b60829ac3253de78897bb67d0
Josh <152639943+Josh5222@users.noreply.github.com>
2024-10-13 03:09:45 -0700

    Added a Few Changes..

--- Main-Folder/theme.js
+++ Main-Folder/theme.js
@@ -1,8 +1,13 @@
 const body = document.body;
+const gradientheader = document.getElementById('gradient-header');
 
 function enableDarkMode(){
     body.classList.remove('light-mode');
     localStorage.setItem('theme', 'dark');
+    gradientheader.style.background = 'linear-gradient(to right, #ffffff, #908e8e)';
+    gradientheader.style.webkitBackgroundClip = 'text';
+    gradientheader.style.color = 'transparent';
+
 }
 
 function enableLightMode(){
